fe2ab561c8002415adb5d8a54e5cba836af119a0
Karina Franco <karinafraest@gmail.com>
2017-06-16 13:45:28 -0500

    median tests pass

--- algorithms/javascript-drills-challenge/src/totals.js
+++ algorithms/javascript-drills-challenge/src/totals.js
@@ -9,3 +9,15 @@ function total(array){
 function mean(array){
 	return total(array)/array.length;
 }
+
+function median(array){
+	var median;
+	array = array.sort();
+	var half = array.length/2
+	if (array.length%2 === 0){
+		median = (array[half -1]+ array[half])/2
+	} else {
+		median = array[Math.floor(half)];
+	};
+	return median;
+}
