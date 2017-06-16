fd44e3e0daa595957729723f1c52e064ad988d0a
Karina Franco <karinafraest@gmail.com>
2017-06-16 13:28:12 -0500

    create mode specs

--- algorithms/javascript-drills-challenge/spec/totalsSpec.js
+++ algorithms/javascript-drills-challenge/spec/totalsSpec.js
@@ -15,20 +15,25 @@ describe("Totals", function(){
 
 	describe("median()", function(){
 		it("calculates the median of an even number of values in the array", function(){
-			var array = [1,2,3,4,5,6]
-			expect(median(array)).toBe(3.5)
+			var array = [1,2,3,4,5,6];
+			expect(median(array)).toBe(3.5);
 		});
 
 		it("calculates the median of an odd number of values in the array", function(){
-			var array = [1,2,3,4,5]
-			expect(median(array)).toBe(3)
+			var array = [1,2,3,4,5];
+			expect(median(array)).toBe(3);
 		});
 
 		it("calculates the median of an even number of values that are not arranged", function(){
-			var array = [6,2,5,1,4,3]
-			expect(median(array)).toBe(3.5)
-		})
+			var array = [6,2,5,1,4,3];
+			expect(median(array)).toBe(3.5);
+		});
 	});
 
-
+	describe("mode()", function(){
+		it("returns an object representing the mode(s) of the array with the property being the mode and the value being the frequency", function(){
+			var array = [1,2,3,2,5];
+			expect(mode(array)).toBe([20]);
+		});
+	});
 });
