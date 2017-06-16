1b9ac41baeb1f37a8f0cca2acb2e5db48794f9be
Karina Franco <karinafraest@gmail.com>
2017-06-16 13:18:52 -0500

    mean spec

--- algorithms/javascript-drills-challenge/spec/totalsSpec.js
+++ algorithms/javascript-drills-challenge/spec/totalsSpec.js
@@ -5,4 +5,11 @@ describe("Totals", function(){
 			expect(total(array)).toBe(30);
 		});
 	});
+
+	describe("mean()", function(){
+		it("calculates the mean of the values in the array", function(){
+			var array = [1,2,2,5,5];
+			expect(mean(array)).toBe()
+		})
+	})
 });
