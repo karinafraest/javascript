abcfcada327599b412f1e36ac05f18a81c3f26d5
Karina Franco <karinafraest@gmail.com>
2017-06-16 13:12:52 -0500

    add more tests to triangle, pass

--- algorithms/javascript-drills-challenge/spec/trianglesSpec.js
+++ algorithms/javascript-drills-challenge/spec/trianglesSpec.js
@@ -1,12 +1,18 @@
 describe("triangles", function(){
 	describe("validTriangle()", function(){
-		it("returns false if all sides are 0 length", function(){
+		it("expects three arguments", function(){
+			expect(validTriangle.length).toBe(3);
+		})
+		it("returns false if any or all sides are 0 length", function(){
 			expect(validTriangle(0,0,0)).toBe(false);
+			expect(validTriangle(1,1,0)).toBe(false);
+    		expect(validTriangle(1,0,1)).toBe(false);
+    		expect(validTriangle(0,1,1)).toBe(false);
 		});
 
 		it("returns false if no such triangle exists", function(){
 			expect(validTriangle(10,10,100)).toBe(false);
-		})
+		});
 
 		it("returns true if all the sides are of equal length (equilateral)", function(){
 			expect(validTriangle(2,2,2)).toBe(true);
@@ -23,6 +29,5 @@ describe("triangles", function(){
 		it("returns true if it is a valid triangle", function(){
 			expect(validTriangle(4,3,5)).toBe(true);
 		});
-
 	});
 })
