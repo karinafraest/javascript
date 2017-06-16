e5645b185df5957b34b7978d925edfd86a01494b
Karina Franco <karinafraest@gmail.com>
2017-06-16 12:35:03 -0500

    perimeter tests

--- algorithms/javascript-drills-challenge/spec/calculationsSpec.js
+++ algorithms/javascript-drills-challenge/spec/calculationsSpec.js
@@ -10,4 +10,16 @@ describe("calculations", function(){
       expect(area(square)).toBe(400);
     });
   });
+ describe("perimeter()", function(){
+ 	it("calculates the perimeter of a rectangle", function(){
+ 		var rectangle = {width: 10, heigth: 20};
+ 		expect(perimeter(rectangle)).toBe(60);
+ 	});
+
+ 	it("calculates the perimeter of a square", function(){
+ 		var square = {width: 20, heigth: 20};
+ 		expect(perimeter(square)).toBe(80);
+ 	});
+ });
+
 });
