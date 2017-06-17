7733348680d56264ceb2207d2bee0bf9f6296e46
Karina Franco <karinafraest@gmail.com>
2017-06-17 02:08:51 -0500

    more tests for count between

--- algorithms/javascript-drills-challenge/spec/countBetweenSpec.js
+++ algorithms/javascript-drills-challenge/spec/countBetweenSpec.js
@@ -10,5 +10,20 @@ describe("count between", function(){
 		var array = []
 		expect(countBetween(array, 3, 5));
 	});
-	
+
+	it("returns 0 if the upperBound is smaller than the lowerBound", function(){
+		var array = [2,3,4,5,6,7];
+		var lowerBound = 7;
+		var upperBound = 3;
+		expect(countBetween(array, lowerBound, upperBound)).toBe(0);
+	});
+
+	it("counts the number of elements even if the lower and upper bounds are the same", function(){
+		var array = [1,2,1,3,1,4];
+		var lowerBound = 1;
+		var upperBound = 1;
+		expect(countBetween(array, lowerBound, upperBound)).toBe(3);
+	})
+
+
 });
