a2a7d660e3ea2e01022f288f4d01d69a5515f567
Karina Franco <karinafraest@gmail.com>
2017-06-18 06:10:08 -0500

    binary search is evil but tests pass

--- algorithms/javascript-drills-challenge/spec/searchingSpec.js
+++ algorithms/javascript-drills-challenge/spec/searchingSpec.js
@@ -12,4 +12,20 @@ describe("Searching", function(){
 			expect(search(array,item)).toBe(false);
 		});
 	});
+
+	describe("binarySearch()", function(){
+		it("returns the index if the element is in the array", function(){
+			var item = 6;
+			var array = [1,2,3,4,5,6,7];
+			expect(binarySearch(array,item)).toBe(5);
+		});
+
+		it("returns -1 if the element is not in the array", function(){
+			var item = 9;
+			var array = [1,2,3,4,5,6,7];
+			expect(binarySearch(array,item)).toBe(-1);
+		});
+	});
+
+
 })
--- algorithms/javascript-drills-challenge/src/searching.js
+++ algorithms/javascript-drills-challenge/src/searching.js
@@ -5,4 +5,28 @@ function search(array, item){
 		};
 	};
 	return false;
+};
+
+function binarySearch(array, item){
+	array.sort(function(a,b){
+		return a-b;
+	});
+	
+	var index = -1;
+	var min = 0;
+	var max = array.length -1;
+	var middle = (max + min)/2;
+	
+	while(min < max){
+		if(item === middle){
+			index = array.indexOf(middle);
+			return index;
+		}else if(item < middle){
+			max = middle;
+		}else if(item > middle){
+			min = middle;
+		}
+		middle =(max+min)/2
+	}
+	return index;
 }
